45c282e214fa9f0fd83268c49ffc00a4ab4597fe
Omkarrajhans <92718352+Omkarrajhans@users.noreply.github.com>
2021-10-20 08:50:19 +0530

    Create Hackerrank_C++_prob_3.cpp

---
+++ Hackerrank_C++_prob_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long long int li;
+    float fl;
+    char ch;
+    long double db;
+    cin>>i>>li>>ch>>fl>>db;
+    cout<<i<<"\n"<<fixed<<li<<"\n"<<ch<<"\n"<<fixed<<fl<<"\n"<<fixed<<db<<"\n";
+    return 0;
+    
+}
